f6ebea70f0da0297f21db2052f8fd3ced67a2d7b
Andrea Leardini <andrea.leardini@nethesis.it>
2019-06-04 12:13:02 +0200

    ui. Completed about page

--- ui/src/views/About.vue
+++ ui/src/views/About.vue
@@ -16,7 +16,7 @@
 
       <div class="list-group-item info-item">
         <span class="fa fa-globe m-right-sm"></span>
-        <a target="_blank" href="https://github.com/NethServer/nethserver-cockpit-empty-VueJS">{{$t('about.website')}}</a>
+        <a target="_blank" href="https://github.com/NethServer/nethserver-nut">{{$t('about.website')}}</a>
       </div>
       <div class="list-group-item info-item">
         <span class="fa fa-bug m-right-sm"></span>
@@ -52,7 +52,7 @@ export default {
         ["system-apps/read"],
         {
           action: "info",
-          name: "nethserver-cockpit-empty-VueJS"
+          name: "nethserver-nut"
         },
         null,
         function(success) {

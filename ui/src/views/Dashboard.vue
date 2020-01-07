31eb844d56ee14b7b1836ca05f634f52f33762d3
Federico Ballarini <fed.ballarini@gmail.com>
2020-01-07 09:46:13 +0100

    ui. update dashboard title and app name (#20)
    NethServer/dev#6004

--- ui/src/views/Dashboard.vue
+++ ui/src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>{{$t('dashboard.title')}}</h1>
+    <h2>{{$t('dashboard.title')}}</h2>
 
     <div class="col-lg-12">
 
--- ui/src/views/Settings.vue
+++ ui/src/views/Settings.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>{{$t('settings.title')}}</h1>
+    <h2>{{$t('settings.title')}}</h2>
     <div v-if="!configLoaded" class="spinner spinner-lg"></div>
     <div v-if="configLoaded">
       <form class="form-horizontal" v-on:submit.prevent="btSaveClick">
